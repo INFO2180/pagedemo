9ca9ef9d4726900075a2e7e9ebcc41107a52746e
David Bain <david@alteroo.com>
2014-10-17 09:52:33 -0500

    initial commit

---
+++ maze.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  
+  
+    var boundaries = $$(".boundary");
+
+    for (var i = 0; i < boundaries.length; i++) {
+      boundaries[i].style.backgroundColor = "orange";
+    }
+  
+  
+  
+}
